10fa8b6d7efa9c648a63bf5aa5a5cc22c456dac1
PIXORA24 <pixora24@gmail.com>
2026-01-06 19:44:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,29 @@ const closeBtn = document.querySelector(".close");
 document.querySelectorAll(".video-click").forEach(card => {
   card.addEventListener("click", () => {
     const vimeoId = card.dataset.vimeo;
+    const youtubeId = card.dataset.youtube;
 
-    container.innerHTML = `
-      <iframe 
-        src="https://player.vimeo.com/video/${vimeoId}?autoplay=1"
-        frameborder="0"
-        allow="autoplay; fullscreen"
-        allowfullscreen>
-      </iframe>
-    `;
+    if (vimeoId) {
+      container.innerHTML = `
+        <iframe
+          src="https://player.vimeo.com/video/${vimeoId}?autoplay=1"
+          frameborder="0"
+          allow="autoplay; fullscreen"
+          allowfullscreen>
+        </iframe>
+      `;
+    }
+
+    if (youtubeId) {
+      container.innerHTML = `
+        <iframe
+          src="https://www.youtube.com/embed/${youtubeId}?autoplay=1&rel=0"
+          frameborder="0"
+          allow="autoplay; fullscreen"
+          allowfullscreen>
+        </iframe>
+      `;
+    }
 
     modal.style.display = "flex";
     document.body.style.overflow = "hidden";
